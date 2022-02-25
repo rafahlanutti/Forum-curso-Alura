88a6afc477bc78b1e29a5f50f2e791aa5900dba6
rafahlanutti <rafahlanutti@gmail.com>
2022-02-24 21:11:26 -0300

    Buscas personalizadas JPA

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -1,15 +1,15 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.dtos.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.TopicoRepository;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -20,9 +20,16 @@ public class TopicoController {
     @Autowired
     TopicoRepository topicoRepository;
 
-  @GetMapping
-    public List<TopicoDto> lista() {
-        var topicos = topicoRepository.findAll();
+    @GetMapping
+    public List<TopicoDto> lista(@RequestParam String nomeCurso) {
+
+        List<Topico> topicos;
+        if (Strings.isEmpty(nomeCurso)) {
+            topicos = topicoRepository.findAll();
+        } else {
+            topicos = topicoRepository.findByCursoNome(nomeCurso);
+        }
+
         return TopicoDto.TopicoDtoConverter.converter(topicos);
     }
 
--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -2,6 +2,22 @@ package br.com.alura.forum.repository;
 
 import br.com.alura.forum.modelo.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface TopicoRepository extends JpaRepository<Topico,Long> {
+import java.util.List;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+    List<Topico> findByCursoNome(String nomeCurso);
+
+    /**
+     *
+     * Quando se tem problema com ambiguidade ex: Topico.cursoNome Topico.curso.nome
+     * O "_" diz ao spring data que tem que buscar topico.curso.nome e não topico.cursoNome
+     */
+    List<Topico> findByCurso_Nome(String nomeCurso);
+
+    @Query("SELEC * FROM Topico t WHERE t.curso.nome = :nomeCurso")
+    List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
 }
