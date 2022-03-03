eb4078b25475b4739969869f3a69363307dd52fc
rafahlanutti <rafahlanutti@gmail.com>
2022-03-02 21:31:10 -0300

    GET detalhes

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.dtos.DetalheTopicoDto;
 import br.com.alura.forum.dtos.TopicoDto;
 import br.com.alura.forum.dtos.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
@@ -42,5 +43,12 @@ public class TopicoController {
         topicoRepository.save(topico);
         var uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
         return ResponseEntity.created(uri).body(new TopicoDto(topico));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DetalheTopicoDto> detalhar(@PathVariable("id") Long id) {
+        var finded = topicoRepository.findById(id).get();
+        var dto = new DetalheTopicoDto(finded);
+        return ResponseEntity.ok(dto);
     }
 }
---
+++ src/main/java/br/com/alura/forum/dtos/DetalheTopicoDto.java
@@ -0,0 +1,59 @@
+package br.com.alura.forum.dtos;
+
+import br.com.alura.forum.modelo.StatusTopico;
+import br.com.alura.forum.modelo.Topico;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DetalheTopicoDto {
+
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao;
+    private String nomeAutor;
+    private StatusTopico statusTopico;
+    private List<RespostaDto> respostas;
+
+    public DetalheTopicoDto(Topico topico) {
+        this.id = topico.getId();
+        this.titulo = topico.getTitulo();
+        this.mensagem = topico.getMensagem();
+        this.dataCriacao = topico.getDataCriacao();
+        this.statusTopico = topico.getStatus();
+        this.nomeAutor = topico.getAutor().getNome();
+        this.respostas = Collections.unmodifiableList(topico.getRespostas().stream().map(RespostaDto::new).collect(Collectors.toList()));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public StatusTopico getStatusTopico() {
+        return statusTopico;
+    }
+
+    public List<RespostaDto> getRespostas() {
+        return respostas;
+    }
+}
