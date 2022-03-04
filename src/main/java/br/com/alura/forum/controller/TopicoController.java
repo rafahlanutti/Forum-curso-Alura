4899e3b09b9d60c3fe99dc5e5116dd86bd2c483c
rafahlanutti <rafahlanutti@gmail.com>
2022-03-03 21:04:04 -0300

    Paginação default

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -32,7 +33,7 @@ public class TopicoController {
     CursoRepository cursoRepository;
 
     @GetMapping
-    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, Pageable paginacao ) {
+    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "id", direction =  Sort.Direction.ASC, page = 0, size = 10) Pageable paginacao ) {
 
         Page<Topico> topicos;
         if (Strings.isEmpty(nomeCurso)) {
