3844f4034fa8bf4123bea53a004767b8de22cf1b
rafahlanutti <rafahlanutti@gmail.com>
2022-03-22 20:52:58 -0300

    actuator

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+@EnableWebMvc
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableCaching
