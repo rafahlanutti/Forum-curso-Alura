2ba613ef2ed8dc5b22b798153f212cb9f253e024
rafahlanutti <rafahlanutti@gmail.com>
2022-03-29 21:48:06 -0300

    profiles e testes

--- src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -4,6 +4,7 @@ import br.com.alura.forum.dtos.LoginDto;
 import br.com.alura.forum.dtos.TokenDto;
 import br.com.alura.forum.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.AuthenticationException;
@@ -16,6 +17,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
+@Profile(value = {"prod", "test"})
 public class AutenticacaoController {
 
 
--- src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.alura.forum.forum;
+package br.com.alura.forum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
