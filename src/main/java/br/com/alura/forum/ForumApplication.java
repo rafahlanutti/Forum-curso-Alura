c13e53ccf5dcf69ea7c09716d43c87e5678f8eb0
rafahlanutti <rafahlanutti@gmail.com>
2022-03-31 20:27:28 -0300

    Update dockerfile

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -10,6 +10,7 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableCaching
+
 public class ForumApplication {
 
 	public static void main(String[] args) {
